e778bbb9c1718f3cc4831b7512b6c1ddcac258ea
yasserprogamer <71287199+yasserprogamer@users.noreply.github.com>
2022-02-08 21:20:44 +0100

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo -e "\033]2;P.O.S.I-SW - Package Online Service Installer Software (Installer mode)\x07"
+echo "P.O.S.I-SW Installer mode."
+echo ""
+echo "Downloading and installing python packages...."
+pip install -r requirements.txt
+echo "Successfully downloaded and installed required python packages!"
+echo ""
+read -s -n 1 -p "Press any key to continue . . ."
