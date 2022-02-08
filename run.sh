2c0448c61bb20c2f2f22e3d5fdb14b5d3ebbfb6b
yasserprogamer <71287199+yasserprogamer@users.noreply.github.com>
2022-02-08 19:32:12 +0100

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo -e "\033]2;P.O.S.I-SW - Package Online Server Installer Software\x07"
+echo "[38;2;00;255;00mPackage Online Service Installer Software (Running mode).[0m"
+echo "[38;2;00;255;255mCreated by: yasserprogamer.[0m"
+echo ""
+python ./src/main.py
